d574ca5a4247df37a4bbd3aa71806a9e3eb05935
Claudiu Cjc <cojoclaudiu@gmail.com>
2021-09-16 19:47:37 +0100

    small change on toggle color

--- components/FindVeganDish.js
+++ components/FindVeganDish.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Switch, StyleSheet} from 'react-native';
 import TextScheme from './TextScheme';
-import {screenWidth} from '../utilis/screenSize';
-import {greenWaga, itemThumbnail} from '../utilis/appColors';
+import {greenWaga, itemThumbnail, mainBackground} from '../utilis/appColors';
+import * as Animatable from 'react-native-animatable';
 
 export default function FindVeganDish({colorScheme, toggle, setSwitch}) {
   return (
-    <View style={styles(colorScheme).toggleContainer}>
+    <Animatable.View
+      animation="fadeIn"
+      style={styles(colorScheme).toggleContainer}>
       <TextScheme fontSize={14} fontWeight="500">
         Find vegan dishes
       </TextScheme>
@@ -15,12 +17,12 @@ export default function FindVeganDish({colorScheme, toggle, setSwitch}) {
           style={{transform: [{scaleX: 0.9}, {scaleY: 0.9}]}}
           trackColor={{false: '#767577', true: greenWaga}}
           ios_backgroundColor="#3e3e3e"
-          thumbColor="white"
+          thumbColor={mainBackground[colorScheme]}
           onChange={setSwitch}
           value={toggle}
         />
       </View>
-    </View>
+    </Animatable.View>
   );
 }
 
@@ -33,7 +35,7 @@ const styles = colorScheme =>
       margin: 10,
       paddingHorizontal: 20,
       paddingVertical: 10,
-      borderRadius: (screenWidth / 6) * 0.2237,
+      borderRadius: 15,
     },
 
     switchContainer: {
